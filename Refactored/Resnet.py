1923b56e1e52691b11a11cd7d4b8b6a0f8f3a990
maxlegrec1 <maxtinos2508@gmail.com>
2023-09-07 22:13:21 +0200

    Ajout legal_prob

--- Refactored/Resnet.py
+++ Refactored/Resnet.py
@@ -26,28 +26,33 @@ enc_vocab_size = 12*64 # Vocabulary size for the encoder
 input_seq_length = 32  # Maximum length of the input sequence
 transformer = Encoder(enc_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
 
-def create_A0(num_residuals):
+def create_A0(num_residuals,use_transformer = True):
+
     vocab = tf.keras.layers.Input(shape=(32))
     mask = tf.keras.layers.Input(shape=(32,32))
-    y = transformer(vocab, mask, True)
-    y = tf.keras.layers.Flatten()(y)
     input = tf.keras.Input(shape=(8, 8, 14))
+
     x = input
     x = tf.keras.layers.Conv2D(256, (3, 3), padding='same')(x)
+
     for _ in range(num_residuals):
         x = residual(x, 256)
     x = tf.keras.layers.Conv2D(64, (3, 3), padding='same',activation='relu')(x)
     x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.concatenate([x,y])
-    x = tf.keras.layers.Dense(8192,activation='relu')(x)
-    x = tf.keras.layers.Dense(8192,activation='relu')(x)
+
+    if use_transformer:
+        y = transformer(vocab, mask, True)
+        y = tf.keras.layers.Flatten()(y)
+        x = tf.keras.layers.concatenate([x,y])
+
+    x = tf.keras.layers.Dense(4096,activation='relu')(x)
     x = tf.keras.layers.Dense(4096,activation='relu')(x)
     x = tf.keras.layers.Softmax()(x)
     output = x
     return tf.keras.Model(inputs=[input,vocab,mask], outputs=output)
 
 
-generator = create_A0(40)
+generator = create_A0(25,use_transformer=False)
 generator.summary()
 
 batch_size = 32
@@ -71,9 +76,9 @@ print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 #custom training loop
 @tf.function
-def train_step(batch,model,metric5,metric10,masked5,masked10,metric1,masked1,stockfish):
+def train_step(batch,model,metric5,metric10,masked5,masked10,metric1,masked1):
     with tf.GradientTape() as tape:
-        x,y_true,mask,y_stock = batch
+        x,y_true,mask = batch
         y_pred = model(x)
         false_moves = (1-mask)*y_pred
         loss1 = tf.keras.losses.categorical_crossentropy(y_true,y_pred)
@@ -82,7 +87,7 @@ def train_step(batch,model,metric5,metric10,masked5,masked10,metric1,masked1,sto
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-
+        lm = tf.reduce_sum(mask*y_pred,axis=-1)
         masked_pred = tf.keras.layers.multiply([y_pred,mask])
         metric5.update_state(y_true,y_pred)
         metric10.update_state(y_true,y_pred)
@@ -93,8 +98,7 @@ def train_step(batch,model,metric5,metric10,masked5,masked10,metric1,masked1,sto
         metric1.update_state(y_true,y_pred)
         masked1.update_state(y_true,masked_pred)
 
-        stockfish.update_state(y_stock,y_true)
-        return loss
+        return loss,lm
 
 
 
@@ -107,19 +111,18 @@ def train(num_step, generator):
     metric1 = tf.keras.metrics.TopKCategoricalAccuracy(k=1, name = "Accuracy")
     metric5 = tf.keras.metrics.TopKCategoricalAccuracy(k=5, name = "top5")
     metric10 = tf.keras.metrics.TopKCategoricalAccuracy(k=10,name = "top10")
-    stockfish = tf.keras.metrics.SparseCategoricalAccuracy(name = "Stockfish")
     #create a log file where we will store the results. It shall be named after the current date and time
     log_file = open(f"Refactored/logs/log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt", "w")
 
     for epoch in range(10000):
         total_loss = 0
+        Legal_prob = 0
         metric5.reset_state()
         metric10.reset_state()
         masked5.reset_state()
         masked10.reset_state()
         masked1.reset_state()
         metric1.reset_state()
-        stockfish.reset_state()
         if epoch%40==0:
             #save weights
             generator.save_weights(f"generator_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{epoch}.h5")
@@ -127,9 +130,11 @@ def train(num_step, generator):
 
         for step in range(num_step):
             batch = next(gen)
-            loss = train_step(batch, generator, metric5, metric10,masked5,masked10,metric1,masked1,stockfish)
+            loss,lm = train_step(batch, generator, metric5, metric10,masked5,masked10,metric1,masked1)
             loss = tf.reduce_mean(loss)
+            lm = tf.reduce_mean(Legal_prob)
             total_loss = total_loss / (step + 1) * step + loss / (step + 1)
+            Legal_prob = Legal_prob / (step + 1) * step + lm / (step + 1)
 
             accuracy = metric1.result().numpy()
             metric5_result = metric5.result().numpy()
@@ -137,17 +142,16 @@ def train(num_step, generator):
             masked5_result = masked5.result().numpy()
             masked10_result = masked10.result().numpy()
             masked1_result = masked1.result().numpy()
-            stockfish_result = stockfish.result().numpy()
             # Use carriage return to overwrite the current line
             print(
-                f"Step: {step}, Loss: {total_loss:.4f}, Acc: {accuracy:.4f}, Masked-Acc: {masked1_result:.4f}, "
+                f"Step: {step}, Loss: {total_loss:.4f}, Acc: {accuracy:.4f}, Masked-Acc: {masked1_result:.4f}, Legal_prob: {Legal_prob:.4f}, "
                 f"Top-5: {metric5_result:.4f}, Masked-5: {masked5_result:.4f}, "
-                f"Top-10: {metric10_result:.4f}, Masked-10: {masked10_result:.4f}, Stockfish: {stockfish_result:.4f}",
+                f"Top-10: {metric10_result:.4f}, Masked-10: {masked10_result:.4f}, ",
                 end="\r"  # Return to the beginning of the line
             )
         # Write the results to the log file
         log_file.write(
-            f"Epoch: {epoch + 1}, Loss: {total_loss:.4f}, Acc: {accuracy:.4f}, Masked-Acc: {masked1_result:.4f}, "
+            f"Epoch: {epoch + 1}, Loss: {total_loss:.4f}, Acc: {accuracy:.4f}, Masked-Acc: {masked1_result:.4f}, Legal_prob: {Legal_prob:.4f}, "
             f"Top-5: {metric5_result:.4f}, Masked-5: {masked5_result:.4f}, "
             f"Top-10: {metric10_result:.4f}, Masked-10: {masked10_result:.4f}\n"
         )
