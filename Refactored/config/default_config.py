f12622264636dccf629459663e37c7004e81f3e3
IAntoineV <antoinevialle.ia@gmail.com>
2023-12-07 20:33:29 +0100

    merge

--- Refactored/config/default_config.py
+++ Refactored/config/default_config.py
@@ -4,7 +4,7 @@ def parameters() -> dict:
     return {
         "training_args" :  {"num_step": 1000, 'warmup_steps':2},
         
-        "shared_parameters" : {"batch_size" : 64, 'lr_start': 1e-4, 'lr':1e-4, 'num_filters': 512, 'num_residuals': 6, 
+        "shared_parameters" : {"batch_size" : 256, 'lr_start': 1e-4, 'lr':1e-4, 'num_filters': 1024, 'num_residuals': 12, 
                                "mini_res_channels" : 64,
                                "path_pgn": "human2.pgn",
                                "num_iters":5},
